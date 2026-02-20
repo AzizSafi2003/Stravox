420b043f5f4a07429f14b4da3f99fca245daf543
Abdul Aziz <aziz20031110@gmail.com>
2026-02-21 03:38:38 +0430

    initial commit

---
+++ backend/src/config/env.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  quiet: true,
+}); /* quiet: true to hide warnings or the custom messages that you have this much environment variables blah blah blah :) */
+
+export const ENV = {
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  FRONTEND_URL: process.env.FRONTEND_URL,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+};
