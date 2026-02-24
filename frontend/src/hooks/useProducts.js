b8a0661117c7efd5d28273e32cf1d2f31e267416
Abdul Aziz <aziz20031110@gmail.com>
2026-02-25 01:41:00 +0430

    profile page, product page added with comments functionality and also delete functionality

--- frontend/src/hooks/useProducts.js
+++ frontend/src/hooks/useProducts.js
@@ -1,5 +1,11 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { createProduct, getAllProducts } from "../lib/api";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getMyProducts,
+  getProductById,
+} from "../lib/api";
 
 export const useProducts = () => {
   const { data, isLoading, error } = useQuery({
@@ -13,5 +19,37 @@ export const useProducts = () => {
 export const useCreateProduct = () => {
   return useMutation({
     mutationFn: createProduct,
+  });
+};
+
+export const useProduct = (id) => {
+  return useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(id),
+    enabled:
+      !!id /* Double bang operator --> which only run the query if there is an ID. `id` itself is a string we convert it into boolean using `!!id`. */,
+  });
+};
+
+export const useDeleteProduct = () => {
+  return useMutation({
+    mutationFn: deleteProduct,
+  });
+};
+
+export const useMyProducts = () => {
+  return useQuery({ queryKey: ["myProducts"], queryFn: getMyProducts });
+};
+
+export const useUpdateProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateProduct,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+      queryClient.invalidateQueries({ queryKey: ["product", variables.id] });
+      queryClient.invalidateQueries({ queryKey: ["myProducts"] });
+    },
   });
 };
